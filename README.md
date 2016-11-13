5aa082de6446cb7a12e733178775be0d46bec2d0
logustra <logustra@gmail.com>
2016-11-13 23:04:50 +0700

    first push

---
+++ README.md
@@ -0,0 +1,24 @@
+# WPS Semarang
+This is the repository for WPS Semarang Project
+
+## Instructions
+
+1. Make sure you have these installed
+	- [node.js](http://nodejs.org/)
+	- [git](http://git-scm.com/)
+	- [grunt](http://gruntjs.com/)
+	- [ruby](https://www.ruby-lang.org/)
+
+2. Clone this repository into your local machine using the terminal (linux) or Gitbash (PC) `git clone https://github.com/Logustra/wpsSemarang.git`
+3. CD to the folder cd `wpsSemarang`
+4. Run `npm-install` to install the project dependencies
+5. Install grunt.js via the linux terminal or Gitbash on a PC `npm install -g grunt-cli`
+5. Run the Grunt command `grunt`
+
+##Notes
+
+1.
+2.
+3.
+4.
+5.
---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt){
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.initConfig ({
+    uglify: {
+      my_target: {
+        files : {
+          "assets/js/scripts.js" : ["assets/builds/js/*.js"]
+        }//files
+      }//my_target
+    },//uglify
+    sass: {
+      options: {
+        outputStyle: "expanded",
+      },//options
+      dev: {
+        files: {
+          "assets/builds/css/styles.css": ["assets/builds/sass/*.scss", "assets/builds/sass/abstracts/*.scss", "assets/builds/sass/base/*.scss", "assets/builds/sass/components/*.scss", "assets/builds/sass/layout/*.scss", "assets/builds/sass/modules/*.scss"]
+        }//files
+      }//dist
+    },//sass
+    autoprefixer: {
+      options: {
+        browser: ["lates 2 versions", "ie 11", "edge", "firefox", "safari", "opera", "chrome"]
+      },
+      my_target: {
+        src: "assets/builds/css/styles.css",
+        dest: "assets/css/styles.css"
+      }
+    },
+    watch : {
+      options: { livereload: true },
+      scripts: {
+        files : ["assets/builds/js/*.js"],
+        tasks : ["uglify"]
+      },//scripts
+      sass: {
+        files : ["assets/builds/sass/*.scss", "assets/builds/sass/abstracts/*.scss", "assets/builds/sass/base/*.scss", "assets/builds/sass/components/*.scss", "assets/builds/sass/layout/*.scss", "assets/builds/sass/modules/*.scss"],
+        tasks : ["sass"]
+      },//sass
+      html : {
+        files: ["*.html"]
+      },//html
+      autoprefixer: {
+        files: ["assets/builds/css/styles.css"],
+        tasks: ["autoprefixer"]
+      },
+    }//watch
+  });//initConfig
+  grunt.registerTask("default", "watch");
+};
